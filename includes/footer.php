9d0098702a09aa4c99aee5cc6554e95092504eba
rishita2605 <64982040+rishita2605@users.noreply.github.com>
2021-09-28 19:10:11 +0530

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -8,7 +8,7 @@
   <div class="right-footer">
     Project by Rishita Raha
     &nbsp;|&nbsp; 
-    <a class="icon" href="https://github.com/rtlearn/php-rishita2605" target="_blank">
+    <a class="icon" href="https://github.com/rishita2605/Captain-Seb-s-Comics" target="_blank">
       Find this Project on Github
       <span class="iconify" data-icon="fa-brands:github-alt" style="font-size: 1.7em; margin-left:0.2em;"></span>
     </a>
